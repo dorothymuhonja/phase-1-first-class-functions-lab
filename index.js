3a31254f8b153a8a4dc4352b54de13b4dbcb1966
dorothymuhonja <dorothymuhonja7@gmail.com>
2022-06-15 02:03:16 +0300

    commit tree e25c080a950874c1c46d5f1cc0cb7f37494aa356

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function() {
+    return ['Antonia', 'Nuru']
+}
+
+const returnLastTwoDrivers = function() {
+    return ['Amari', 'Mo']
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(fare) {
+
+    return function fareMultiplier(num) {
+        return fare * num
+    }
+     
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers([], returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers()
+
+}
+
+
+
+
+
+
